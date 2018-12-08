e7762b4bfab113bf116e010a8ca3fbec8d582139
bigocb@gmail.com <bigocb@gmail.com>
2018-12-08 17:07:17 -0500

    e

--- src/App.vue
+++ src/App.vue
@@ -227,7 +227,11 @@ export default {
                         removeTags: {},
             newTag: {},
             model: {},
-            summary: {},
+            summary: {
+                summary: ""
+
+                
+            },
             posts: [],
             postsapproved: [],
             search: '',
