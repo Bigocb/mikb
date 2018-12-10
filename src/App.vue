502c4202d65fe7b2468d515d688936ccda2448dc
Bobby Cloutier <bobby.cloutier@gmail.com>
2018-12-10 12:07:50 -0500

    s

--- src/App.vue
+++ src/App.vue
@@ -270,8 +270,8 @@ export default {
     computed: {
         filteredList() {
                  this.email = this.account.user.email
-      this.family = await api.getFamily(this.email)
-            this.posts = await api.getPosts(this.family.familyid)
+      this.family =  api.getFamily(this.email)
+            this.posts =  api.getPosts(this.family.familyid)
             return this.posts.filter(post => {
                 return post.title.toLowerCase().includes(this.search.toLowerCase())
             })
